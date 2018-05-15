d061c9329e931c93382ade56ba1a33732c8d5324
Paul Tunbridge <tunbridgep@gmail.com>
2018-05-15 16:33:49 +1000

    Added a lot more console logging and got rid of dism default output

--- new_pc_installation.bat
+++ new_pc_installation.bat
@@ -12,53 +12,71 @@ if NOT %errorLevel% == 0 (
 
 
 ::Disable Hybernation
+echo Disabling Hybernation...
 powercfg.exe /hibernate off
 
 ::disable UAC
+echo Disabling UAC...
 reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 
 ::show file extensions
+echo Turning on Always Show File Extensions...
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
 
 ::show hidden files
+echo Turning on Always Show Hidden Files...
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
 
 ::show all icons in notification area tray 
+echo Turning on Show All Icons in Notification Area Tray...
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer /v EnableAutoTray /t REG_DWORD /d 0 /f
 
 ::always show "Open CMD Here" option to context menus
+echo Adding "Open CMD Here" to context menus...
 reg delete HKEY_CLASSES_ROOT\Directory\shell\cmd /v Extended /f
 reg delete HKEY_CLASSES_ROOT\Drive\shell\cmd /v Extended /f
 reg delete HKEY_CLASSES_ROOT\Directory\Background\shell\cmd /v Extended /f
 
 ::disable useless windows features
-Dism /online /disable-feature /NoRestart /FeatureName:"InboxGames"
-Dism /online /disable-feature /NoRestart /FeatureName:"MediaPlayback"
-Dism /online /disable-feature /NoRestart /FeatureName:"TabletPCOC"
-Dism /online /disable-feature /NoRestart /FeatureName:"Printing-Foundation-InternetPrinting-Client"
-Dism /online /disable-feature /NoRestart /FeatureName:"FaxServicesClientPackage"
-Dism /online /disable-feature /NoRestart /FeatureName:"Xps-Foundation-Xps-Viewer"
-Dism /online /disable-feature /NoRestart /FeatureName:"Printing-XPSServices-Features"
-Dism /online /disable-feature /NoRestart /FeatureName:"WindowsGadgetPlatform"
+echo Disabling Windows Games...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"InboxGames"
+echo Disabling Media Playback (media player, media center, etc)...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"MediaPlayback"
+echo Disabling Tablet Feeatures...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"TabletPCOC"
+echo Disabling Internet Printing Client...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Printing-Foundation-InternetPrinting-Client"
+echo Disabling Fax Services Client...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"FaxServicesClientPackage"
+echo Disabling XPS Viewer and XPS Printing...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Xps-Foundation-Xps-Viewer"
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Printing-XPSServices-Features"
+echo Disabling Windows Gadget Platform...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"WindowsGadgetPlatform"
 
 ::remove Internet Explorer
-Dism /online /disable-feature /NoRestart /FeatureName:"Internet-Explorer-Optional-amd64"
-Dism /online /disable-feature /NoRestart /FeatureName:"Internet-Explorer-Optional-x86"
-Dism /online /disable-feature /NoRestart /FeatureName:"Internet-Explorer-Optional-ia64"
+echo Disabling Internet Explorer...
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Internet-Explorer-Optional-amd64"
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Internet-Explorer-Optional-x86"
+Dism /online /disable-feature /NoRestart /quiet /FeatureName:"Internet-Explorer-Optional-ia64"
 ::start /w ocsetup Internet-Explorer-Optional-x86 /uninstall
 
 ::Disable Windows Defender
+echo Disabling Windows Defender...
 sc stop windefend
 sc delete windefend
 
 ::Disable "Scan and Fix" for thumb drives
+echo Disabling "Scan and Fix" for thumb drives...
 sc stop ShellHWDetection
 sc delete ShellHWDetection
 
 ::Disable Open File Security Warnings
+echo Disabling Open File Security Warnings...
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Internet Explorer\Security" /v DisableSecuritySettingsCheck /t REG_DWORD /d 1 /f
 
 ::Disable "Enhance Pointer Precision" to disable mouse accelleration in games
+echo Turning off "Enhanced Pointer Precision"...
 reg add "HKCU\Control Panel\Mouse" /v MouseSpeed /t REG_SZ /d 0 /f
 reg add "HKCU\Control Panel\Mouse" /v MouseThreshhold1 /t REG_SZ /d 0 /f
 reg add "HKCU\Control Panel\Mouse" /v MouseThreshhold2 /t REG_SZ /d 0 /f
